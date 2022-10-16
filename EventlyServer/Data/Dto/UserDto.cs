81336a4a8f6627169414b9a4a8fbca08cfc7861a
Artem Zhikharev <artemziharew@gmail.com>
2022-10-16 18:30:11 +0300

    Рефакторинг UserDto
    В c#9-10 версии ввели record, которые представляют собой неизменяемый класс, который можно сравнивать по значению.
    Во многом это аналог data class из Kotlin.
    Так что record идеально подходит для создания DTO

--- EventlyServer/Data/Dto/UserDto.cs
+++ EventlyServer/Data/Dto/UserDto.cs
@@ -1,21 +1,4 @@
 ﻿namespace EventlyServer.Data.Dto;
 
-public class UserDto
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string? PhoneNumber { get; set; }
-    public string? OtherCommunication { get; set; }
-    public bool IsAdmin { get; set; }
-
-    public UserDto(string name, string email, string password, string? phoneNumber, string? otherCommunication = null, bool isAdmin = false)
-    {
-        Name = name;
-        Email = email;
-        Password = password;
-        PhoneNumber = phoneNumber;
-        OtherCommunication = otherCommunication;
-        IsAdmin = isAdmin;
-    }
-}
+public record UserDto(string Name, string Email, string Password, string? PhoneNumber,
+    string? OtherCommunication = null, bool IsAdmin = false);
