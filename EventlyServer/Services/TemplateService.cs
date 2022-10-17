a16073128417fc3ff991f453a78011cad5748c84
Artem Zhikharev <artemziharew@gmail.com>
2022-10-17 21:25:49 +0300

    TemplateService обозначен как нуждающийся в доработке
    Нужно будет позже им заняться

--- EventlyServer/Services/TemplateService.cs
+++ EventlyServer/Services/TemplateService.cs
@@ -5,6 +5,13 @@ using EventlyServer.Data.Repositories.Abstracts;
 
 namespace EventlyServer.Services;
 
+/* TODO: Исправить шаблон
+    Шаблон представляет собой физический файл на сервере. 
+    Для работы с ним необходимо передавать клиенту и получать от него непосредственно файл.
+    Эту информацию нужно изучить и дописать потом эту логику.
+*/
+
+
 public class TemplateService
 {
     private readonly IRepository<Template> _templateRepository;
@@ -16,7 +23,8 @@ public class TemplateService
 
     public async Task<List<TemplateDto>> ShowTemplates()
     {
-        var templates = await _templateRepository.GetAllAsync();
-        return templates.ConvertAll(item => item.ToDto());
+        throw new NotImplementedException();
+        // var templates = await _templateRepository.GetAllAsync();
+        // return templates.ConvertAll(item => item.ToDto());
     }
 }
