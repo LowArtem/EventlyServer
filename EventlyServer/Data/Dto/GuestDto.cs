a650ac73b965ed5c3c32c04e7d7b9097ddc39005
Artem Zhikharev <artemziharew@gmail.com>
2022-10-29 23:33:25 +0300

    Добавлена документация GuestDto

--- EventlyServer/Data/Dto/GuestDto.cs
+++ EventlyServer/Data/Dto/GuestDto.cs
@@ -1,5 +1,30 @@
-﻿namespace EventlyServer.Data.Dto;
+﻿using System.ComponentModel.DataAnnotations;
 
-public record GuestDto(int Id, string Name);
+namespace EventlyServer.Data.Dto;
+
+/// <summary>
+/// Гость, принявший приглашение на мероприятие
+/// </summary>
+public record GuestDto
+{
+    /// <summary>
+    /// Гость, принявший приглашение на мероприятие
+    /// </summary>
+    /// <param name="id">ID гостя</param>
+    /// <param name="name">Имя</param>
+    public GuestDto(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    /// <summary>ID гостя</summary>
+    [Required]
+    public int Id { get; init; }
+
+    /// <summary>Имя</summary>
+    [Required]
+    public string Name { get; init; }
+}
 
 public record GuestFullCreatingDto(string Name, string PhoneNumber, int IdInvitation);
