a5f926796fa2819c8d394a2e2cb89f9acf2df919
Artem Zhikharev <artemziharew@gmail.com>
2022-10-30 13:46:41 +0300

    Добавлены кастомные исключения

---
+++ EventlyServer/Exceptions/EntityExistsException.cs
@@ -0,0 +1,11 @@
+﻿namespace EventlyServer.Exceptions;
+
+/// <summary>
+/// Исключение обозначает, что объект с такими характеристиками уже сущестует и второй не может быть создан
+/// </summary>
+public class EntityExistsException : Exception
+{
+    public EntityExistsException(string? message) : base(message)
+    {
+    }
+}
---
+++ EventlyServer/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace EventlyServer.Exceptions;
+
+/// <summary>
+/// Исключение обозначает, что необходимый объект не был найден
+/// </summary>
+public class EntityNotFoundException : Exception
+{
+    public EntityNotFoundException(string? message) : base(message)
+    {
+    }
+}
