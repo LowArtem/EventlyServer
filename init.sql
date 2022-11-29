d1698840c37f0519826682689a8f471b08333d3e
Artem Zhikharev <artemziharew@gmail.com>
2022-11-29 14:22:28 +0300

    В скрипт инициализации БД добавлена инициализация данными

--- init.sql
+++ init.sql
@@ -62,3 +62,17 @@ before insert
 on guests
 for each row
 execute procedure guests_insert();
+
+
+-- Insert test data
+
+
+insert into users (name, email, password, phone_number, is_admin) values
+('Goga', 'goga@top.com', 'gogagoga', '89999999999', false);
+
+insert into types_of_event (name) values
+('birthday');
+
+
+insert into templates (name, price, file_path, preview_path, id_type_of_event) values
+('Template666', 2500, 'C:/file1.html', 'C:/file2.png', 1);
