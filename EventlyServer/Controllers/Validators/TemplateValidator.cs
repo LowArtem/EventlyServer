97be6cb7c7ed4fdaf81b3700b16ba3180bab2078
Artem Zhikharev <artemziharew@gmail.com>
2022-12-26 20:16:05 +0300

    Увеличен доступный размер поля PreviewPath в Template

--- EventlyServer/Controllers/Validators/TemplateValidator.cs
+++ EventlyServer/Controllers/Validators/TemplateValidator.cs
@@ -11,7 +11,7 @@ public class TemplateValidator : AbstractValidator<TemplateCreatingDto>
         RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
         RuleFor(x => x.Price).NotEmpty().GreaterThanOrEqualTo(0);
         RuleFor(x => x.FilePath).NotEmpty().Must(x => x.ValidateAsFilePath()).MaximumLength(100);
-        RuleFor(x => x.PreviewPath).NotEmpty().Must(x => x.ValidateAsFilePath()).MaximumLength(100);
+        RuleFor(x => x.PreviewPath).NotEmpty().Must(x => x.ValidateAsFilePath()).MaximumLength(300);
         RuleFor(x => x.IdEvent).NotEmpty().GreaterThan(0);
     }
 }
