432ad1a4b86af7846a22fea49a5a574370979239
Artem Zhikharev <artemziharew@gmail.com>
2023-01-09 11:16:07 +0300

    fix(.github): передача env var на удаленный хост

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,4 @@ mkdir ~/.ssh
 echo "$CI_DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook -i ansible/hosts.txt --private-key ~/.ssh/id_rsa ansible/deploy_configs.yml
+ansible-playbook -i ansible/hosts.txt --extra-vars "GITHUB_WORKSPACE=$GITHUB_WORKSPACE" --private-key ~/.ssh/id_rsa ansible/deploy_configs.yml
