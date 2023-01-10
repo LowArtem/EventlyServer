4bc9ad781352e77954903ecd563d6ceaa66f0037
Artem Zhikharev <artemziharew@gmail.com>
2023-01-10 18:47:14 +0300

    fix(.github): все extra vars в одной строке

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,6 @@ mkdir ~/.ssh
 echo "$CI_DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook -i ansible/hosts.txt --extra-vars "extra_vars.yml" --private-key ~/.ssh/id_rsa $INPUT_PLAYBOOKPATH
+ansible-playbook -i ansible/hosts.txt \
+--extra-vars "GITHUB_WORKSPACE=$GITHUB_WORKSPACE DOPPLER_TOKEN=$DOPPLER_TOKEN DOCKER_USERNAME=$DOCKER_USERNAME IMAGE_NAME=$IMAGE_NAME IMAGE_VERSION=$IMAGE_VERSION" \
+--private-key ~/.ssh/id_rsa $INPUT_PLAYBOOKPATH
