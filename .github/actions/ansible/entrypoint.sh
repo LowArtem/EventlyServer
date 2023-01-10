8fda47a93d0710bb95b81ea7d632c43ff961b1e4
Artem Zhikharev <artemziharew@gmail.com>
2023-01-10 15:24:24 +0300

    refactor: github actions, workflows уточнение работы
    - ancible action может запускать разные файлы
    - Переименован workflow для деплоя на сервер (потому что собранный image тоже деплоится)
    - Переписан рестарт сервера: используется ansible вместо запроса к watchtower
    - включена перезагрузка докера absible'ом

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -4,4 +4,4 @@ mkdir ~/.ssh
 echo "$CI_DEPLOY_SSH_KEY" > ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
 
-ansible-playbook -i ansible/hosts.txt --extra-vars "GITHUB_WORKSPACE=$GITHUB_WORKSPACE DOPPLER_TOKEN=$DOPPLER_TOKEN" --private-key ~/.ssh/id_rsa ansible/deploy_configs.yml
+ansible-playbook -i ansible/hosts.txt --extra-vars "GITHUB_WORKSPACE=$GITHUB_WORKSPACE DOPPLER_TOKEN=$DOPPLER_TOKEN" --private-key ~/.ssh/id_rsa $INPUT_PLAYBOOKPATH
